9ef63c3859e7927851a38f576cfb66f3481e53a1
WestonNyirenda <wordweston64@gmail.com>
2024-09-10 21:39:51 +0200

    incldeded notifications, events(emails)

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,6 +13,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
